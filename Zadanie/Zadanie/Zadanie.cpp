87fc69078316ee2f8d9e9e2f19468ab7fdc23dbc
harbungenTA <bartej2008@gmail.com>
2020-03-16 12:43:15 +0100

    Zad

---
+++ Zadanie/Zadanie/Zadanie.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int a;
+
+int main()
+{
+	cout << "Podaj swoj wiek: ";
+	cin >> a;
+	if (a >= 18)
+	{
+		cout << "Mozesz glosowac w wyborach";
+	}
+	else
+	{
+		cout << "Nie mozesz glosowac w wyborach";
+	}
+
+	return 0;
+}
