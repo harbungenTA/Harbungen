ed1411142d3a580deff218485bbe2da4c2a8db53
harbungenTA <bartej2008@gmail.com>
2020-03-18 12:56:38 +0100

    Zadanie 18.03
    Bartosz Mucha 1GTP G2

---
+++ ZAD/ZAD/ZAD.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int a;
+int main()
+{
+	int tab[121];
+	for (int i = 0; i < 121; i++)
+	{
+		tab[i] = 0;
+	}
+		cout << "Podaj liczbe od 1-119:";
+		cin >> a;
+			tab[a] = 100;
+			tab[a - 1] = 10;
+			tab[a + 1] = 10;
+            for (int i = 119; i >= 0; i--)
+            {
+                if (tab[i] == '121')
+                {
+                    cout << " ";
+                }
+                else
+                {
+                    cout << tab[i];
+                }
+            }
+
+
+
+
+ return 0;
+}
