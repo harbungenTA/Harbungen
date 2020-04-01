658fc2e0bdfbd36402efbf396308b39a45a2295c
harbungenTA <bartej2008@gmail.com>
2020-04-01 10:32:16 +0200

    zadanie
    Bartosz Mucha

---
+++ ConsoleApplication3/ConsoleApplication3/ConsoleApplication3.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+#include <iostream>
+#include <fstream>   
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    int a;
+    string t[100];
+    int lic = 0;
+    ifstream plik;
+    plik.open("D:\\plik.txt");
+    if (plik.good())
+
+        while (!plik.eof())
+        {
+            t[lic] = a;
+            plik >> a;
+            cout << lic++ << ":" << a;
+            cout << endl;
+        }
+    plik.close();
+
+    for (int i = 0; i < lic; i++)
+        cout << lic << endl;
+   
+    return 0;
+}
