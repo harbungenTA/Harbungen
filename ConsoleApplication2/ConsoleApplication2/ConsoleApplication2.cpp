2c103e864e80baa37711a280497f12e58a29c57b
harbungenTA <bartej2008@gmail.com>
2020-04-01 10:20:33 +0200

    zadanie
    Bartosz Mucha

---
+++ ConsoleApplication2/ConsoleApplication2/ConsoleApplication2.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+int main() {
+	
+	string tab[100];
+	
+    string slowa;
+    ifstream plik;
+
+    plik.open("D:\\plik.txt");
+    if (plik.good() == true)
+    {
+        while (!plik.eof())
+        {
+   
+            for (int i = 0; i < 100 && plik >> slowa ; ++i) {
+                tab[i];
+            }
+            cout << tab << endl;
+        }
+        plik.close();
+    }
+
+
+	return 0;
+}
