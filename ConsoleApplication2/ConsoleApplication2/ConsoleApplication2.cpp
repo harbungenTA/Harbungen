ae8c085245cc031b387b89201475e01c94274e00
harbungenTA <bartej2008@gmail.com>
2020-04-08 10:12:37 +0200

    Zadanie 08.04.20

---
+++ ConsoleApplication2/ConsoleApplication2/ConsoleApplication2.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int liczba;
+
+
+int main() {
+
+	ifstream wejscie;
+	wejscie.open("D:\\wej.txt");
+
+	ofstream wyjscie;
+	wyjscie.open("D:\\wyj.txt");
+
+	if(wejscie.good())
+		while (!wejscie.eof())
+			{
+			wejscie >> liczba;
+			if (liczba % 2 == 0)
+				wyjscie << liczba << " ";
+			
+			}
+
+	wyjscie.close();
+	wejscie.close();
+
+	return 0;
+}	
