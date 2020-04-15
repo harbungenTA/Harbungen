3fb34f3d5bea8d57b68e9b9515a4b77d43618c22
harbungenTA <bartej2008@gmail.com>
2020-04-15 10:27:10 +0200

    Zad 15.04.20
    Bartosz Mucha

---
+++ ConsoleApplication3/ConsoleApplication3/ConsoleApplication3.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main()
+{
+    int tab[1000];
+    int x = 0;
+
+    ifstream wej;
+        wej.open("wej.txt");
+    ofstream wyj;
+        wyj.open("dwa.txt");
+
+                                                                             //By Bartosz Mucha
+    if (wej.good())
+        while (!wej.eof()) {
+            wej >> tab[x];
+            x++;
+        }
+
+    wej.close();
+
+
+    for (int i = x - 1; i >= 0; i--)
+    {
+        wyj << tab[i];
+    }
+
+    wyj.close();
+
+}
+
+                
