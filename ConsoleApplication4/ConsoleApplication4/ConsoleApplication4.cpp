f6314bdb38f2aa25d625734364690bc848d19a48
harbungenTA <bartej2008@gmail.com>
2020-04-22 11:21:46 +0200

    zad 22.4.20
    22.4.20

---
+++ ConsoleApplication4/ConsoleApplication4/ConsoleApplication4.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    string imie;
+    string nazwisko;
+    string id;
+    string linia;
+
+
+    ofstream wyjscie;
+    ifstream wejscie;
+
+    wejscie.open("wej.txt");
+    wyjscie.open("wyj.json");
+    if (wejscie.good())
+        while (!wejscie.eof() && getline(wejscie, linia))
+        {
+
+            getline(wejscie, linia);
+            wyjscie << "\n { \n \"imie\" : \" " << linia << "\"";
+
+            getline(wejscie, linia);
+            wyjscie << "  \n\"nazwisko\" : \" " << linia << "\"";
+
+            getline(wejscie, linia);
+            wyjscie << "  \n\"id\" : \" " << linia << "\"\n},";
+
+
+        }
+
+    wejscie.close();
+    wyjscie.close();
+
+    return 0;
+}
