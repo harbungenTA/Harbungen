af7712d52142a207a35f3ec91d2de86eec1a1135
harbungenTA <bartej2008@gmail.com>
2020-04-29 10:17:52 +0200

    zad 29.04

---
+++ zad29.04/zad29.04/zad29.04.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main()
+{
+    ifstream wej;
+    ofstream wyj;
+
+    wej.open("wej.txt");
+    wyj.open("wyj.json");
+
+    int tab[1000];
+    int x = 0;
+
+    if (wej.good()) {
+        wyj << "[";
+        while (!wej.eof()) {
+            wej >> tab[x++];
+        }
+
+        while (x != -1) {
+
+            wyj << " {  \"liczba\" : \"" << tab[x--]<<"\"}";
+
+            if (!wej.eof()) {
+
+                wyj << ",";
+
+            }
+
+        }
+        wyj << "]";
+    }
+
+    wej.close();
+    wyj.close();
+
+    return 0;
+}
