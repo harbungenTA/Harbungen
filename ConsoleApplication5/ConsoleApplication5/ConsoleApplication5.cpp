d87e1705225d6f849174f41c0b62bfa5bd4fc641
harbungenTA <bartej2008@gmail.com>
2020-05-06 10:16:45 +0200

    zad 06.05.20

---
+++ ConsoleApplication5/ConsoleApplication5/ConsoleApplication5.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <fstream>
+#include <cstdlib>
+
+using namespace std;
+
+int main() {
+
+	int t[100];
+
+	ifstream wej;
+	wej.open("wej.txt");
+	ofstream wyj;
+	wyj.open("wyj.json");
+
+	if (wej.good()) {
+		while (!wej.eof())
+
+			for (int i = 0; i < 10; i++)
+				wej >> t[i];
+	}
+
+	int x = t[0];
+
+	for (int i = 0; i < 10; i++) {
+		if (x < t[i])
+			x = t[i];
+	}
+
+	wyj << "[ \n { \n \"najwieksza\" : \" " << x;
+
+	wej.close();
+	wyj.close();
+
+	return 0; 
+}
