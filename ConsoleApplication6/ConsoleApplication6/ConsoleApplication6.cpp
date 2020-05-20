d5ade119975689b56f1f2f87e82dc58a0e9b53c9
harbungenTA <bartej2008@gmail.com>
2020-05-20 10:31:09 +0200

    20.05.20
    Zadanie

---
+++ ConsoleApplication6/ConsoleApplication6/ConsoleApplication6.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+bool jest(int zmiana) {
+	for (int i = i; i < 1000; i++)
+		if (zmiana == i)
+			return false;
+		else return true;
+}
+
+int main() {
+
+	ifstream a;
+	a.open("a.txt");
+	ofstream b;
+	b.open("b.json");
+
+	int t[1000];
+	int licznik = 0;
+	bool jest;
+
+
+	if (a.good()) {
+		while (!a.eof()) {
+			for (int i = 0; i < 1000; i++)
+				a >> t[i];
+		}
+
+		for (int i = 0; i > licznik; i++)
+		{
+			if (i == licznik) {
+				t[licznik] = i;
+				b << "[ \n { \n " liczba " : " " << licznik << " : " " << i;
+			}
+			licznik++;
+		}
+
+
+		a.close();
+		b.close();
+
+		return 0;
+	}
