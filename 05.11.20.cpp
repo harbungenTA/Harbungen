c037919b4b72d06de343790621d90d8d51839e99
harbungenTA <62106234+harbungenTA@users.noreply.github.com>
2020-11-05 10:42:50 +0100

    Add files via upload

---
+++ 05.11.20.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <bitset>
+
+using namespace std;
+
+unsigned a, b, c, d, e, f, g;
+
+int main() {
+
+	cout << "Podaj a: ";
+	cin >> a;
+	cout << "\n";
+	cout << "Podaj b: ";
+	cin >> b;
+	cout << "\n";
+
+	c = a | b;
+	d = a & b;
+	e = a ^ b;
+	f = a>>=b;
+	g = a<<=b;
+	cout << bitset<8>(c)<<endl;
+	cout << bitset<8>(d)<<endl;
+	cout << bitset<8>(e)<<endl;
+	cout << bitset<8>(f)<<endl;
+	cout << bitset<8>(g);
+	
+	return 0;
+}
