e1955137530c2c931c50b746a074e0d54a38af21
harbungenTA <62106234+harbungenTA@users.noreply.github.com>
2020-11-19 11:01:58 +0100

    11.19.20

---
+++ plik/ConsoleApplication2.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <bitset>
+#include <fstream>
+#include <math.h>
+
+using namespace std;
+
+int licz1, licz2;
+
+int zerujBit(int liczba, int nrBitu) {
+    return (~(0b100000 << nrBitu) & liczba);
+}
+
+int main()
+{
+    ifstream plik;
+    plik.open("a.txt");
+
+    ofstream plik2;
+    plik2.open("b.txt");
+ 
+    if (plik.good())
+    {
+        while (!plik.eof()) {
+            plik >> licz1 >> licz2;
+            plik2 << zerujBit(licz1, licz2) << endl;
+            cout << zerujBit(licz1, licz2) << endl;
+        }
+
+    }
+
+    plik.close();
+    plik2.close();
+
+    return 0;
+}
