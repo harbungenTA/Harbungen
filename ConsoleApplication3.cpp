d01d69c14f5954a3335f3579feef8b2f707a5738
harbungenTA <62106234+harbungenTA@users.noreply.github.com>
2020-11-26 10:47:49 +0100

    11/26/20

---
+++ ConsoleApplication3.cpp
@@ -0,0 +1,29 @@
+﻿#include<iostream>
+#include<cstdlib>
+#include<fstream>
+
+using namespace std;
+
+int main()
+{
+    ifstream liczby;
+    liczby.open("liczby.txt");
+
+    int licz;
+
+    if(liczby.good())
+        while (!liczby.eof()) {
+            liczby>>licz;
+            cout << "\n";
+            for (int i = 2; i <= licz; i++) {
+                while (licz % i == 0) {
+                    cout << i << " ";
+                    licz /= i;
+                }
+            }
+        }
+    liczby.close();
+    cout << "\n";
+    system("pause");
+    return 0;
+}
