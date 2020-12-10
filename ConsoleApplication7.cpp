74b37b4adfeb016eb35afebeeca874ad694b1d64
harbungenTA <62106234+harbungenTA@users.noreply.github.com>
2020-12-10 10:17:42 +0100

    12.10.20

---
+++ ConsoleApplication7.cpp
@@ -0,0 +1,50 @@
+﻿#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+class plik {
+	ifstream wej;
+	ofstream wyj;
+public:
+	plik();
+	~plik();
+	void loop();
+};
+
+plik::plik() {
+	wej.open("liczby.txt");
+	wyj.open("c:\\wynik.txt");
+}
+
+plik::~plik() {
+	wej.close();
+	wyj.close();
+}
+
+void plik::loop() {
+	int a[];
+	int licznik = 0;
+	while (1000) {
+		for (int i = 0;i < 200; i++)
+		{
+			wej >> a[i];
+			if (a[i] < 1000) {
+				a[] >> wyj;
+				licznik++;
+			}
+		}
+		wyj << " ";
+		wyj << a[999];
+		wyj << " ";
+		wyj << a[1000];
+	}
+}
+
+int main() {
+
+	plik dane;
+	dane.loop();
+
+	return 1;
+}
