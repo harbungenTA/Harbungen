d2416feed80c7c6837f0fddccb936dfb5f359270
harbungenTA <62106234+harbungenTA@users.noreply.github.com>
2021-02-11 11:38:34 +0100

    02/11/20

---
+++ ConsoleApplication3.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <math.h>
+#include <fstream>
+#include <cstdlib>
+#include <stdlib.h>
+
+using namespace std;
+
+int liczenie(int number) {
+
+    int licz6 = 0; 
+
+    while (number > 0)
+    {
+        if (number % 10 == 6)
+            licz6++;
+        number /= 10;
+    }
+    return licz6;
+}
+
+
+int osiem(string liczba) {
+    int os = strtol(liczba.c_str(), NULL, 8);
+    return os;
+}
+
+int main() {
+
+    cout << osiem << liczenie(166);
+}
